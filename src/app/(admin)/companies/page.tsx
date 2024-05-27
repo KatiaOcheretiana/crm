51c78b0e6c638267c458a4817af69dde1c8900db
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-05-27 16:03:58 +0300

    react query / company info

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import Header from '@/app/components/header';
-import Toolbar from '../../components/toolbar';
-import SearchInput from '../../components/search-input';
-import AddCompanyButton from '../../components/add-company-button';
-import CompanyTable from '../../components/company-table';
-import CompanyRow from '../../components/company-row';
-import { Status } from '../../components/status-label';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { getCompanies } from '@/lib/api';
+import getQueryClient from '@/lib/utils/getQueryClient';
+import CompanyTable from '@/app/components/company-table';
 
-export interface PageProps {
-  params: { id: string };
-}
+export interface PageProps {}
 
-export default function Page({ params }: PageProps) {
+export default async function Page({}: PageProps) {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies({ cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
-      <CompanyTable>
-        <CompanyRow
-          id={1}
-          category="Products"
-          company="Costco"
-          status={Status.Pending}
-          promotion={true}
-          country="USA"
-          joinedDate="02.19.2023"
-        />
-        {/* <CompanyRow
-          id={2}
-          category="Products"
-          company="АТБ"
-          status={Status.Pending}
-          promotion={false}
-          country="UK"
-          joinedDate="02.19.2023"
-        /> */}
-      </CompanyTable>
-    </>
+    <HydrationBoundary state={dehydratedState}>
+      <CompanyTable />
+    </HydrationBoundary>
   );
 }
